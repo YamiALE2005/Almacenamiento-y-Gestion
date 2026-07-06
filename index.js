fe8dbda913471c8b0acdf06cad942d6ee173bb13
Yamilet Martinez Delgado <st4546@utr.edu.mx>
2026-07-06 12:50:51 -0600

    CRUD

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-require('dotenv').config();
+require("dotenv").config();
+
+const express = require("express");
 const conectarDB = require("./src/config/database");
+const expedienteRoutes = require("./src/routes/expedienteRoutes");
+
 conectarDB();
-const express = require('express');
 
 const app = express();
-const PORT = 5100;
+const port = process.env.PORT || 5100;
 
-app.listen(PORT, () => {
-    console.log('Hello World');
+app.use(express.json());
+
+app.use("/api", expedienteRoutes);
+
+app.listen(port, () => {
+    console.log(`Servidor ejecutándose en el puerto ${port}`);
 });
